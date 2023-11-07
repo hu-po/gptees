fa4a4426bffceb5bdbf854920ddfd2695c381597
hu.po <hugo.p.cmu@gmail.com>
2023-11-07 07:19:05 -0600

    working gpt4v example

---
+++ gpt4v.py
@@ -0,0 +1,52 @@
+"""
+https://platform.openai.com/docs/guides/vision
+"""
+import base64
+
+import os
+import requests
+
+# OpenAI API Key
+api_key = os.environ["OPENAI_API_KEY"]
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "/home/oop/Downloads/thumbnail.png"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "What’s in this image?"
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+          }
+        ]
+      }
+    ],
+    "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
