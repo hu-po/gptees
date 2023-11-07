17a5dc009318f2b337b9fbff9f8d886d6bb2d94b
hu.po <hugo.p.cmu@gmail.com>
2023-11-07 07:31:48 -0600

    talk to webcam

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gptees
 Playing with new GPT APIs
+
+```
+pip install openai==1.1.1
+pip install pydub==0.25.1
+```
--- gpt4v.py
+++ gpt4v.py
@@ -1,52 +1,99 @@
 """
 https://platform.openai.com/docs/guides/vision
 """
-import base64
-
 import os
+import base64
+import cv2
 import requests
+import io
+from openai import OpenAI
+from pydub import AudioSegment
+from pydub.playback import play
 
-# OpenAI API Key
-api_key = os.environ["OPENAI_API_KEY"]
+client = OpenAI()
+cap = cv2.VideoCapture(0)
 
-# Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Path to your image
-image_path = "/home/oop/Downloads/thumbnail.png"
+def capture_image_from_webcam():
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+    ret, frame = cap.read()  # Capture frame-by-frame
+    cap.release()  # When everything done, release the capture
+    cv2.destroyAllWindows()
+    if ret:
+        return frame
+    else:
+        raise ValueError("Could not capture an image from the webcam")
 
-# Getting the base64 string
-base64_image = encode_image(image_path)
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
-}
+def encode_image_from_webcam():
+    img = capture_image_from_webcam()
 
-payload = {
-    "model": "gpt-4-vision-preview",
-    "messages": [
-      {
-        "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": "What’s in this image?"
-          },
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": f"data:image/jpeg;base64,{base64_image}"
+    # Convert the image to base64
+    _, buffer = cv2.imencode(".jpg", img)
+    jpg_as_text = base64.b64encode(buffer).decode("utf-8")
+
+    return jpg_as_text
+
+
+def encode_image_from_path(img_path):
+    with open(img_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def vision(
+    image_path: str = None,
+    api_key: str = os.environ["OPENAI_API_KEY"],
+    prompt: str = "Describe what you see",
+    max_tokens: int = 24,
+):
+    if image_path is None:
+        base64_image = encode_image_from_webcam()
+    else:
+        base64_image = encode_image_from_path(image_path)
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt,
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
             }
-          }
-        ]
-      }
-    ],
-    "max_tokens": 300
-}
+        ],
+        "max_tokens": max_tokens,
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
+    )
+    return response.json()["choices"][0]["message"]["content"]
 
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
-print(response.json())
+def stream_and_play(text):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text,
+    )
+
+    # Convert the binary response content to a byte stream
+    byte_stream = io.BytesIO(response.content)
+
+    # Read the audio data from the byte stream
+    audio = AudioSegment.from_file(byte_stream, format="mp3")
+
+    # Play the audio
+    play(audio)
+
+
+if __name__ == "__main__":
+    reply = vision()
+    stream_and_play(reply)
