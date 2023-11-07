a8ca55fb320ebed42c7e1d31209083e9bdeb1506
hu.po <hugo.p.cmu@gmail.com>
2023-11-07 09:40:30 -0600

    camera demo

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Playing with new GPT APIs
 ```
 pip install openai==1.1.1
 pip install pydub==0.25.1
+pip install opencv-python==4.8.1.78
+pip install scipy==1.10.1
+pip install sounddevice==0.4.6
 ```
--- gpt4v.py
+++ gpt4v.py
@@ -85,14 +85,8 @@ def stream_and_play(text):
         voice="alloy",
         input=text,
     )
-
-    # Convert the binary response content to a byte stream
     byte_stream = io.BytesIO(response.content)
-
-    # Read the audio data from the byte stream
     audio = AudioSegment.from_file(byte_stream, format="mp3")
-
-    # Play the audio
     play(audio)
 
 
