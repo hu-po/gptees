02939354b4f7fbac41591c58cc4650df23e75349
hu.po <hugo.p.cmu@gmail.com>
2023-11-15 13:01:15 -0600

    this is so gross

---
+++ robot.py
@@ -0,0 +1,83 @@
+robot_tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "move_to",
+            "description": "Move the robot in a specified direction with a rotation",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "direction": {
+                        "type": "string",
+                        "description": "The direction to move in, e.g., forward, backward, left, right",
+                    },
+                    "rotation": {
+                        "type": "number",
+                        "description": "The rotation angle in degrees",
+                    },
+                },
+                "required": ["direction", "rotation"],
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "look_at",
+            "description": "Orient the robot's camera to a specified pan and tilt",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "pan": {
+                        "type": "number",
+                        "description": "Pan angle in degrees",
+                    },
+                    "tilt": {
+                        "type": "number",
+                        "description": "Tilt angle in degrees",
+                    },
+                },
+                "required": ["pan", "tilt"],
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "take_image",
+            "description": "Capture an image using the robot's camera",
+            "parameters": {
+                "type": "object",
+                "properties": {}
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "perform",
+            "description": "Perform a specified action",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "action_name": {
+                        "type": "string",
+                        "description": "The name of the action to perform",
+                    },
+                },
+                "required": ["action_name"],
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_up",
+            "description": "Command the robot to get up",
+            "parameters": {
+                "type": "object",
+                "properties": {}
+            },
+        }
+    },
+]
