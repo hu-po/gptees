acbad4198bc762d839946713cfb7cf84fa3e631e
hu.po <hugo.p.cmu@gmail.com>
2023-11-17 13:30:28 -0600

    clean

--- robot.py
+++ robot.py
@@ -27,105 +27,15 @@ VISION_PROMPT: str = ". ".join(
 MAX_TOKENS_VISION: int = 32  # max tokens for reply
 VISION_OUTPUT_PATH: str = "/tmp/test.png"  # default image for test behavior
 
-
-def encode_image_from_path(image_path: str = VISION_OUTPUT_PATH) -> str:
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
-
-
-def vision(
-    base64_image: str,
-    prompt: str = VISION_PROMPT,
-    vision_model: str = VISION_MODEL,
-    max_tokens: int = MAX_TOKENS_VISION,
-) -> str:
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
-    }
-    payload = {
-        "model": vision_model,
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": prompt},
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
-                    },
-                ],
-            }
-        ],
-        "max_tokens": max_tokens,
-    }
-    response = requests.post(
-        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
-    )
-    return response.json()["choices"][0]["message"]["content"]
-
-
 # Audio models
 TTS_MODEL: str = "tts-1"  # Text-to-speech model
 STT_MODEL: str = "whisper-1"  # Speech-to-text model
 VOICE: str = "echo"  # (alloy, echo, fable, onyx, nova, and shimmer)
+GREETING: str = "hello there" # Greeting is spoken on start
 AUDIO_RECORD_SECONDS: int = 6  # Duration for audio recording
 AUDIO_SAMPLE_RATE: int = 22100  # Sample rate for audio recording
 AUDIO_CHANNELS: int = 1  # mono
 AUDIO_OUTPUT_PATH: str = "/tmp/audio.wav"  # audio is constantly overwritten
-
-
-def listen(
-    duration: int = AUDIO_RECORD_SECONDS,
-    sample_rate: int = AUDIO_SAMPLE_RATE,
-    channels: int = AUDIO_CHANNELS,
-    output_path: str = AUDIO_OUTPUT_PATH,
-) -> str:
-    print(f"Recording for {duration} seconds.")
-    audio_data = sd.rec(
-        int(duration * sample_rate),
-        samplerate=sample_rate,
-        channels=channels,
-    )
-    sd.wait()  # Wait until recording is finished
-    print("Recording finished.")
-    write(output_path, sample_rate, audio_data)  # Save as WAV file
-    with open(output_path, "rb") as audio_file:
-        transcript = CLIENT.audio.transcriptions.create(
-            model=STT_MODEL, file=audio_file, response_format="text"
-        )
-    print(f"Transcript: {transcript}")
-    return f"Listened for {duration} seconds, heard __{transcript}__"
-
-
-def speak(
-    text: str,
-    save_to_file=False,
-    model: str = TTS_MODEL,
-    voice: str = VOICE,
-) -> str:
-    # Check if the file already exists and the save_to_file flag is True
-    if save_to_file:
-        file_name = f"/tmp/test.{text[:10]}.mp3"
-        if os.path.exists(file_name):
-            print(f"Audio already exists at {file_name}")
-            audio = AudioSegment.from_file(file_name, format="mp3")
-    else:
-        # If the file doesn't exist, create the audio and save it if required
-        response = CLIENT.audio.speech.create(model=model, voice=voice, input=text)
-        byte_stream = io.BytesIO(response.content)
-        audio = AudioSegment.from_file(byte_stream, format="mp3")
-
-        # Save the file if save_to_file is True
-        if save_to_file:
-            audio.export(file_name, format="mp3")
-            print(f"Saved audio to {file_name}")
-
-    # Play the audio
-    print(f"Playing audio: {text}")
-    play(audio)
-    return f"Speaking text: {text}"
-
 
 # System model chooses tools and actions to perform based on vision
 SYSTEM_MODEL: str = "gpt-4-1106-preview"
@@ -248,6 +158,95 @@ TOOLS = [
 FUNCTIONS = [tool["function"] for tool in TOOLS]
 
 
+def encode_image_from_path(image_path: str = VISION_OUTPUT_PATH) -> str:
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def vision(
+    base64_image: str,
+    prompt: str = VISION_PROMPT,
+    vision_model: str = VISION_MODEL,
+    max_tokens: int = MAX_TOKENS_VISION,
+) -> str:
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
+    }
+    payload = {
+        "model": vision_model,
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        ],
+        "max_tokens": max_tokens,
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
+    )
+    return response.json()["choices"][0]["message"]["content"]
+
+
+def listen(
+    duration: int = AUDIO_RECORD_SECONDS,
+    sample_rate: int = AUDIO_SAMPLE_RATE,
+    channels: int = AUDIO_CHANNELS,
+    output_path: str = AUDIO_OUTPUT_PATH,
+) -> str:
+    print(f"Recording for {duration} seconds.")
+    audio_data = sd.rec(
+        int(duration * sample_rate),
+        samplerate=sample_rate,
+        channels=channels,
+    )
+    sd.wait()  # Wait until recording is finished
+    print("Recording finished.")
+    write(output_path, sample_rate, audio_data)  # Save as WAV file
+    with open(output_path, "rb") as audio_file:
+        transcript = CLIENT.audio.transcriptions.create(
+            model=STT_MODEL, file=audio_file, response_format="text"
+        )
+    print(f"Transcript: {transcript}")
+    return f"Listened for {duration} seconds, heard __{transcript}__"
+
+
+def speak(
+    text: str,
+    model: str = TTS_MODEL,
+    voice: str = VOICE,
+    save_to_file = True,
+) -> str:
+    # Check if the file already exists and the save_to_file flag is True
+    if save_to_file:
+        file_name = f"/tmp/test.{text[:10]}.mp3"
+        if os.path.exists(file_name):
+            print(f"Audio already exists at {file_name}")
+            audio = AudioSegment.from_file(file_name, format="mp3")
+    else:
+        # If the file doesn't exist, create the audio and save it if required
+        response = CLIENT.audio.speech.create(model=model, voice=voice, input=text)
+        byte_stream = io.BytesIO(response.content)
+        audio = AudioSegment.from_file(byte_stream, format="mp3")
+
+        # Save the file if save_to_file is True
+        if save_to_file:
+            audio.export(file_name, format="mp3")
+            print(f"Saved audio to {file_name}")
+
+    # Play the audio
+    print(f"Playing audio: {text}")
+    play(audio)
+    return f"Speaking text: {text}"
+
+
 def move_to(direction: str) -> str:
     return f"Moving to {direction}"
 
@@ -304,7 +303,7 @@ def choose_tool(
 
 
 if __name__ == "__main__":
-    speak("hello")
+    speak(GREETING)
     base64_image = encode_image_from_path()
     what_i_see = vision(base64_image)
     what_i_hear = listen()
